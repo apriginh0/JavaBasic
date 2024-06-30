7f02abcb5cec92c8a36bf9fa91b5d57d3102cbd7
apriginh0 <apriginh0@hotmail.com>
2024-06-30 03:33:21 -0300

    Atualizando

---
+++ src/TiposVariaveis/TiposVariaveis.java
@@ -0,0 +1,14 @@
+package TiposVariaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args){
+        byte idade = 132;
+        short ano = 2024;
+        int cep = 50620585;
+        long cpf = 7189283463L;
+        float pi = 3.14F;
+        double salario = 10500.90;
+
+        final double VALOR_DE_PI = 3.14; // Constante precisa da palavra "final" e nome em caixa alta
+    }
+}
